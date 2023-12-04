46e25ca19177905293ec8e288d179780bffec170
fagner ferreira <fagnerrc12@gmail.com>
2023-12-04 09:10:39 -0300

    update

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,12 +5,13 @@ namespace SD
     public class Customer
     {
         [Required]
+        [MinLength(10)]
         public required string Name { get; set; }
         [Required]
+        [MinLength(11)]
         public required string Cpf { get; set; }
         public int Id { get => id; }
         public int id;
-
         public List<PaymentMethod> PaymentMethods { get; set; } = new();
 
     }
